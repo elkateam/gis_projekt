e7ca735dd49665fb54e064e2c2584762a4ce13d7
Łukasz <woz.lukasz@gmail.com>
2014-01-02 19:30:59 +0100

    #test

---
+++ src/com/elka/gis/file/reader/TestMain.java
@@ -0,0 +1,10 @@
+package com.elka.gis.file.reader;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GIS projekt");
+	}
+
+}
